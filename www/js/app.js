1d2d5a2ed760f9d540d3c2bdf8ff8d685ac978a5
cwpurdy <christian@aspendigital.com>
2016-11-12 11:07:33 -0700

    added navbar, and report menu

--- www/js/app.js
+++ www/js/app.js
@@ -1,71 +1,98 @@
 // Ionic Starter App
-
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic', 'ngCordova'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  })
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    })
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  $stateProvider
-  .state('map', {
-    url: '/',
-    templateUrl: 'templates/map.html',
-    controller: 'MapCtrl'
-  });
+        $stateProvider
 
-  $urlRouterProvider.otherwise("/");
+            .state('map', {
+            url: '/',
+            templateUrl: 'templates/map.html',
+            controller: 'MapCtrl'
+        })
 
-})
+        .state('report', {
+            url: '/',
+            templateUrl: 'templates/report.html',
+            controller: 'MapCtrl'
+        });
 
-.controller('MapCtrl', function($scope, $state, $cordovaGeolocation) {
-  var options = {timeout: 10000, enableHighAccuracy: true};
+        $urlRouterProvider.otherwise("/");
 
-  $cordovaGeolocation.getCurrentPosition(options).then(function(position){
-    
-    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    })
+    .controller('MapCtrl', function($scope, $state, $cordovaGeolocation) {
+        var options = {
+            timeout: 10000,
+            enableHighAccuracy: true
+        };
 
-    var mapOptions = {
-      center: latLng,
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
+        $scope.goToreport = function() {
+            $state.go("report");
+        }
+        $scope.goTomap = function() {
+            $state.go("map");
+        }
 
-    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+        $cordovaGeolocation.getCurrentPosition(options).then(function(position) {
 
-    //Wait until the map is loaded
-    google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+            var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-      var marker = new google.maps.Marker({
-          map: $scope.map,
-          animation: google.maps.Animation.DROP,
-          position: latLng
-      });      
+            var mapOptions = {
+                center: latLng,
+                zoom: 15,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
 
-      var infoWindow = new google.maps.InfoWindow({
-          content: "Here I am!"
-      });
+            $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-      google.maps.event.addListener(marker, 'click', function () {
-          infoWindow.open($scope.map, marker);
-      });
+            //Wait until the map is loaded
+            google.maps.event.addListenerOnce($scope.map, 'idle', function() {
+
+                var marker = new google.maps.Marker({
+                    map: $scope.map,
+                    animation: google.maps.Animation.DROP,
+                    position: latLng
+                });
+
+                var infoWindow = new google.maps.InfoWindow({
+                    content: "Here I am!"
+                });
+
+                google.maps.event.addListener(marker, 'click', function() {
+                    infoWindow.open($scope.map, marker);
+                });
+
+            });
+
+        }, function(error) {
+            console.log("Could not get location");
+        })
+
+    })
+    .controller('reportCtrl', function($scope, $state, $cordovaGeolocation) {
+
+        $scope.goToreport = function() {
+            $state.go("report");
+        }
+
+        $scope.goTomap = function() {
+            $state.go("map");
+        }
 
     });
-
-  }, function(error){
-    console.log("Could not get location");
-  });
-});
